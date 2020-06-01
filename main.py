68073cb5796a493b04733d8baa74daa54c734340
Thisaja Alwis <thisajaalwis@gmail.com>
2020-05-31 20:50:12 -0400

    branch

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import sys
 sys.path.insert(1,"src")
-import pyautogui
 import time
 import shutil
-import random
-import coordinates
+from coordinates import client,game,clientCoords,gameCoords,computerWidth,computerHeight
 import cv2 as cv
 import numpy as np
-import directInputController
+from directInputController import setPos,click,rightClick,getPixel,pressKey,holdKey,releaseKey
 from windowcapture import WindowCapture
 from directInputController import keyboardButton
 
@@ -21,72 +19,91 @@ def name(img):
     return "src\\images\\"+img+".PNG"
 def locate(img,screenshot,threshold=0.9):
     img=name(img)
-    img=cv.imread(img,cv.IMREAD_UNCHANGED)
+    img=cv.imread(img)
     img=img[...,:3]
     img=np.ascontiguousarray(img)
     result=cv.matchTemplate(screenshot,img,cv.TM_CCOEFF_NORMED)
     min_val,max_val,min_loc,max_loc=cv.minMaxLoc(result)
     if(max_val>=threshold):
-        max_loc=(max_loc[0]+coordinates.clientCoords[0],max_loc[1]+coordinates.clientCoords[1])
+        max_loc=(max_loc[0]+clientCoords[0],max_loc[1]+clientCoords[1])
         return max_loc
     else:
         return None
-def click(location):
-    directInputController.set_pos(location[0],location[1])
-    directInputController.left_click()
-def rightClick(location):
-    directInputController.set_pos(location[0],location[1])
-    directInputController.right_click()
-def pressKey(key):
-    directInputController.press_key(key)
-    time.sleep(0.05)
-    directInputController.release_key(key)
+def locateGame(img,screenshot,lower_range=np.array([0,0,0]),upper_range=np.array([255,255,255]),threshold=0.9):
+    img=name(img)
+    img=cv.imread(img)
+    img=img[...,:3]
+    img=np.ascontiguousarray(img)
 
-def changeCoords(coordinate,screen):
-    if(screen=="client"):
-        return (coordinate[0]+coordinates.clientCoords[0],coordinate[1]+coordinates.clientCoords[1])
-    elif(screen=="game"):
-        return (coordinate[0]+coordinates.gameCoords[0],coordinate[1]+coordinates.gameCoords[1])
+    img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    img=cv.inRange(img,lower_range,upper_range)
+    background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
+    background=cv.inRange(background,lower_range,upper_range)
+
+    result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
+    min_val,max_val,min_loc,max_loc=cv.minMaxLoc(result)
+    if(max_val>=threshold):
+        max_loc=(max_loc[0]+gameCoords[0],max_loc[1]+gameCoords[1])
+        return max_loc
+    else:
+        return None
 def chooseChamp():
+    click(client.chatButton)
+    time.sleep(0.5)
+    pressKey(keyboardButton.M)
+    pressKey(keyboardButton.I)
+    pressKey(keyboardButton.D)
+    pressKey(keyboardButton.Enter)
+    time.sleep(0.5)
     for x in range(755-448,1166-448,82):
         for y in range(430-294,731-294,75):
-            click(changeCoords((x,y),"client"))
+            click((x+clientCoords[0],y+clientCoords[1]))
             time.sleep(0.2)
+    time.sleep(1)
+    click(client.searchButton)
+    time.sleep(0.5)
+    pressKey(keyboardButton.S)
+    pressKey(keyboardButton.I)
+    pressKey(keyboardButton.V)
+    pressKey(keyboardButton.I)
+    pressKey(keyboardButton.R)
+    time.sleep(1)
+    click(client.champButton)
+    time.sleep(1)
+    click(client.lockInButton)
     time.sleep(1)
 def buyItems():
     pressKey(keyboardButton.P)
     time.sleep(1)
-    rightClick(changeCoords(coordinates.game.item1,"game"))
+    rightClick(game.item1)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item2,"game"))
+    rightClick(game.item2)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item3,"game"))
+    rightClick(game.item3)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item4,"game"))
+    rightClick(game.item4)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item5,"game"))
+    rightClick(game.item5)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item6,"game"))
+    rightClick(game.item6)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item7,"game"))
+    rightClick(game.item7)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item8,"game"))
+    rightClick(game.item8)
     time.sleep(0.2)
-    rightClick(changeCoords(coordinates.game.item9,"game"))
+    rightClick(game.item9)
     time.sleep(1)
     pressKey(keyboardButton.Escape)
     time.sleep(5)
-    # rightClick(changeCoords(coordinates.game.tower,"game"))
-    # time.sleep(10)
 def a():
     changeSettings()
-    click(changeCoords(coordinates.client.playButton,"client"))
+    click(client.playButton)
     time.sleep(2)
-    click(changeCoords(coordinates.client.coopButton,"client"))
+    click(client.coopButton)
     time.sleep(2)
-    click(changeCoords(coordinates.client.beginnerButton,"client"))
+    click(client.intermediateButton)
     time.sleep(2)
-    click(changeCoords(coordinates.client.confirmButton,"client"))
+    click(client.confirmButton)
     time.sleep(2)
     time.sleep(1)
 def b():
@@ -98,49 +115,50 @@ def b():
             break
         if(locate("grayLockInButton",screenshot)==None):
             if(locate("findMatchButton",screenshot)!=None):
-                click(changeCoords(coordinates.client.findMatchButton,"client"))
+                click(client.findMatchButton)
             if(locate("acceptButton",screenshot)!=None):
-                click(changeCoords(coordinates.client.acceptButton,"client"))
+                click(client.acceptButton)
         if(locate("chooseChampion",screenshot)!=None):
             chooseChamp()
-            click(changeCoords(coordinates.client.searchButton,"client"))
-            time.sleep(0.5)
-            pyautogui.typewrite("sivir")
-            time.sleep(1)
-            click(changeCoords(coordinates.client.champButton,"client"))
-            time.sleep(1)
-            click(changeCoords(coordinates.client.lockInButton,"client"))
-            time.sleep(1)
+    time.sleep(5)
 def c():
-    while(pyautogui.locateCenterOnScreen(name("minimap"))==None):
-        time.sleep(0.1)
-    print("Now in game")
-    rightClick ((coordinates.computerWidth/2,coordinates.computerHeight/2))
-    time.sleep(1)
     wincap=WindowCapture('League of Legends (TM) Client')
+    while(True):
+        try:
+            screenshot = wincap.get_screenshot()
+        except:
+            break
+        if(locate("minimap",screenshot)!=None):
+            break
+def d():
+    print("Now in game")
+    rightClick((computerWidth/2,computerHeight/2))
+    time.sleep(1)
     q=False
     w=False
     e=False
     pressKey(keyboardButton.Y)
     buyItems()
+    wincap=WindowCapture('League of Legends (TM) Client')
+    looptime=time.time()
     while(True):
         try:
             screenshot = wincap.get_screenshot()
         except:
             break
         
-        if(locate("recallButton",screenshot)==None):
+        if(getPixel(game.recallButton)!=(64, 85, 95)):
             #checking health
-            if(pyautogui.pixelMatchesColor(950,955,(1,13,7))):
-                rightClick(changeCoords(coordinates.game.base,"game"))
-                rightClick(changeCoords(coordinates.game.base,"game"))
+            if(getPixel(game.healthBar)==(1,13,7)):
+                rightClick(game.base)
+                rightClick(game.base)
                 pressKey(keyboardButton.D)
                 pressKey(keyboardButton.F)
                 x=0
                 flag=True
                 for i in range(8):
-                    flag=pyautogui.pixelMatchesColor(950,955,(1,13,7))
-                    rightClick(changeCoords(coordinates.game.base,"game"))
+                    flag=getPixel(game.healthBar)==(1,13,7)
+                    rightClick(game.base)
                     time.sleep(1)
                     if(flag==False):
                         break
@@ -150,34 +168,34 @@ def c():
                     buyItems()
             else:
                 #finding casters
-                lower_range=np.array([111,167,81])
-                upper_range=np.array([121,215,130])
-                img=name("casterMinion")
-                img=cv.imread(img,cv.IMREAD_UNCHANGED)
-                img=img[...,:3]
-                img=np.ascontiguousarray(img)
-                img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
-                img=cv.inRange(img,lower_range,upper_range)
-                background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
-                background=cv.inRange(background,lower_range,upper_range)
-                result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
-                min_val,max_val,min_loc,max_loc=cv.minMaxLoc(result)
-                if(max_val>=0.5):
-                    pyautogui.moveTo(changeCoords(max_loc,"game"))  
+                # lower_range=np.array([111,167,81])
+                # upper_range=np.array([121,215,130])
+                # img=name("casterMinion")
+                # img=cv.imread(img)
+                # img=img[...,:3]
+                # img=np.ascontiguousarray(img)
+                # img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+                # img=cv.inRange(img,lower_range,upper_range)
+                # background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
+                # background=cv.inRange(background,lower_range,upper_range)
+                # result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
+                max_loc=locateGame("casterMinion",screenshot,np.array([111,167,81]),np.array([121,215,130]),0.5)
+                if(max_loc!=None):
+                    setPos(max_loc)  
                     pressKey(keyboardButton.Space)
                 else:
-                    rightClick(changeCoords(coordinates.game.tower,"game"))
+                    rightClick(game.tower)
 
                 #finding champ
                 lower_range=np.array([2,204,165])
                 upper_range=np.array([4,204,166])
                 img=name("champHealth")
-                img=cv.imread(img,cv.IMREAD_UNCHANGED)
+                img=cv.imread(img)
                 img=img[...,:3]
                 img=np.ascontiguousarray(img)
                 img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
-                background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
                 img=cv.inRange(img,lower_range,upper_range)
+                background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
                 background=cv.inRange(background,lower_range,upper_range)
                 result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
                 minChamp_val,maxChamp_val,minChamp_loc,maxChamp_loc=cv.minMaxLoc(result)
@@ -186,23 +204,23 @@ def c():
                 lower_range=np.array([49,0,135])
                 upper_range=np.array([100,40,166])
                 img=name("tower")
-                img=cv.imread(img,cv.IMREAD_UNCHANGED)
+                img=cv.imread(img)
                 img=img[...,:3]
                 img=np.ascontiguousarray(img)
                 img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
-                background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
                 img=cv.inRange(img,lower_range,upper_range)
+                background=cv.cvtColor(screenshot,cv.COLOR_BGR2HSV)
                 background=cv.inRange(background,lower_range,upper_range)
-                result=cv.matchTemplate(background,img,cv.TM_SQDIFF)
+                result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
                 min_val,max_val,min_loc,max_loc=cv.minMaxLoc(result)
                 
                 #checking if theres a enemy champion
                 if(maxChamp_val>=0.5):
                     #checking if you are not in tower range
-                    if(max_val<=30000000.0):
+                    if(max_val<=0.5):
                         pressKey(keyboardButton.T)
-                        maxChamp_loc=(maxChamp_loc[0]+50,maxChamp_loc[1]+75)
-                        pyautogui.moveTo(changeCoords(maxChamp_loc,"game"))
+                        maxChamp_loc=(maxChamp_loc[0]+gameCoords[0]+50,maxChamp_loc[1]+gameCoords[1]+75)
+                        setPos(maxChamp_loc)
                         pressKey(keyboardButton.E)
                         pressKey(keyboardButton.R)
                         pressKey(keyboardButton.Q)
@@ -214,7 +232,7 @@ def c():
                     
                 #levelling up
                 if(locate("levelUpButton",screenshot)!=None):
-                    directInputController.press_key(keyboardButton.Lctrl)
+                    holdKey(keyboardButton.Lctrl)
                     time.sleep(0.05)
                     pressKey(keyboardButton.R)
                     if(q==False):
@@ -229,10 +247,12 @@ def c():
                     pressKey(keyboardButton.Q)
                     pressKey(keyboardButton.W)
                     pressKey(keyboardButton.E )
-                    directInputController.release_key(keyboardButton.Lctrl)
+                    releaseKey(keyboardButton.Lctrl)
         else:
             buyItems()
-def d():
+        print("fps:",(1/(time.time()-looptime)))
+        looptime=time.time()
+def e():
     time.sleep(20)
     wincap = WindowCapture('League of Legends')
     while(True):
@@ -243,67 +263,77 @@ def d():
         if(locate("playButton",screenshot)!=None):
             break
         if(locate("honor",screenshot)!=None):
-            pyautogui.click(changeCoords(coordinates.client.honorButton,"client"))
+            click(client.honorButton)
+        if(locate("dailyPlay",screenshot,0.8)!=None):
+            click(client.dailyPlayButton)
         if(locate("okButton",screenshot,0.8)!=None):
-            pyautogui.click(locate("okButton",screenshot,0.8))
+            click(locate("okButton",screenshot,0.8))
         if(locate("xButton",screenshot,0.8)!=None):
-            pyautogui.click(locate("xButton",screenshot,0.8))
-# def nothing(x):
-#     pass
-# cv.namedWindow("Tracking")
-# cv.createTrackbar("LH","Tracking",0,255,nothing)
-# cv.createTrackbar("LS","Tracking",0,255,nothing)
-# cv.createTrackbar("LV","Tracking",0,255,nothing)
-# cv.createTrackbar("UH","Tracking",255,255,nothing)
-# cv.createTrackbar("US","Tracking",255,255,nothing)
-# cv.createTrackbar("UV","Tracking",255,255,nothing)
-# def e():
+            click(locate("xButton",screenshot,0.8))
+# # def nothing(x):
+# #     pass
+# # cv.namedWindow("Tracking")
+# # cv.createTrackbar("LH","Tracking",0,255,nothing)
+# # cv.createTrackbar("LS","Tracking",0,255,nothing)
+# # cv.createTrackbar("LV","Tracking",0,255,nothing)
+# # cv.createTrackbar("UH","Tracking",255,255,nothing)
+# # cv.createTrackbar("US","Tracking",255,255,nothing)
+# # cv.createTrackbar("UV","Tracking",255,255,nothing)
+# def f():
 # #     lh=cv.getTrackbarPos("LH","Tracking")
 # #     ls=cv.getTrackbarPos("LS","Tracking")
 # #     lv=cv.getTrackbarPos("LV","Tracking")
 # #     uh=cv.getTrackbarPos("UH","Tracking")
 # #     us=cv.getTrackbarPos("US","Tracking")
 # #     uv=cv.getTrackbarPos("UV","Tracking")
+
+#     looptime=time.time()
 #     while True:
 #         wincap=WindowCapture('League of Legends (TM) Client')
 #         try:
 #             screenshot = wincap.get_screenshot()
 #         except:
 #             break
-#     # lower_range=np.array([lh,ls,lv])
-#     # upper_range=np.array([uh,us,uv])
-#         lower_range=np.array([111,167,81])
-#         upper_range=np.array([121,215,130])
-#         img=name("casterMinion")
-#         img=cv.imread(img,cv.IMREAD_UNCHANGED)
+#         # lower_range=np.array([lh,ls,lv])
+#         # upper_range=np.array([uh,us,uv])
+#         lower_range=np.array([49,0,135])
+#         upper_range=np.array([100,40,166])
+#         img=name("tower")
+#         img=cv.imread(img)
 #         img=img[...,:3]
 #         img=np.ascontiguousarray(img)
 #         img=cv.cvtColor(img,cv.COLOR_BGR2HSV)
 #         img=cv.inRange(img,lower_range,upper_range)
 
-#         background=name("background")
-#         background=cv.imread(background,cv.IMREAD_UNCHANGED)
+#         background=name("test")
+#         background=cv.imread(background)
 #         background=background[...,:3]
-#         background=np.ascontiguousarray(background)
+#         background=np.ascontiguousarray(background) 
 #         background=screenshot
 #         background=cv.cvtColor(background,cv.COLOR_BGR2HSV)
 #         background=cv.inRange(background,lower_range,upper_range)
+        
+
 
 #         result=cv.matchTemplate(background,img,cv.TM_CCOEFF_NORMED)
 #         min_val,max_val,min_loc,max_loc=cv.minMaxLoc(result)
 
 #         print(max_val)
-
+        
 #         result=cv.bitwise_and(background,background,result)
 #         cv.rectangle(result,max_loc,(max_loc[0]+50,max_loc[1]+50),color=(255,255,255),thickness=2)
 
 #         cv.imshow("result",result)
-#         cv.waitKey(1)#4,161,600 ->
+#         cv.waitKey(1)
+#         #4,161,600 ->
+#         # print("fps",(1/(time.time()-looptime)))
+#         # looptime=time.time()
 def main():
     while True:
         a()
         b()
         c()
         d()
+        e()
 
-main()
+d()
--- src/directInputController.py
+++ src/directInputController.py
@@ -1,11 +1,12 @@
 import os
 import ctypes
 import win32api
+import PIL.ImageGrab
+import pydirectinput
 import time
-from win32api import GetSystemMetrics
 
-computerWidth=GetSystemMetrics(0)
-computerHeight=GetSystemMetrics(1)
+computerWidth=win32api.GetSystemMetrics(0)
+computerHeight=win32api.GetSystemMetrics(1)
 class MouseButton:
    class up:
       left= 0x0004
@@ -17,48 +18,48 @@ class MouseButton:
       right = 0x0008
 
 class keyboardButton:
-    n1=0x02
-    n2=0x03
-    n3=0x04
-    n4=0x05
-    n5=0x06
-    n6=0x07
-    n7=0x08
-    n8=0x09
-    n9=0x0A
-    n0=0x0B
-    Q=0x10
-    W=0x11
-    E=0x12
-    R=0x13
-    T=0x14
-    Y=0x15
-    U=0x16
-    I=0x17
-    O=0x18
-    P=0x19
-    Delete = 0xD3 + 1024
-    A=0x1E
-    S=0x1F
-    D=0x20
-    F=0x21
-    G=0x22
-    H=0x23
-    J=0x24
-    K=0x25
-    L=0x26
-    Enter = 0x1C
-    Z=0x2C
-    X=0x2D
-    C=0x2E
-    V=0x2F
-    B=0x30
-    N=0x31
-    M=0x32
-    Period = 0x34
-    Lctrl= 0x1D
-    Space = 0x39
-    Escape = 0x01
+   n1=0x02
+   n2=0x03
+   n3=0x04
+   n4=0x05
+   n5=0x06
+   n6=0x07
+   n7=0x08
+   n8=0x09
+   n9=0x0A
+   n0=0x0B
+   Q=0x10
+   W=0x11
+   E=0x12
+   R=0x13
+   T=0x14
+   Y=0x15
+   U=0x16
+   I=0x17
+   O=0x18
+   P=0x19
+   Delete = 0xD3 + 1024
+   A=0x1E
+   S=0x1F
+   D=0x20
+   F=0x21
+   G=0x22
+   H=0x23
+   J=0x24
+   K=0x25
+   L=0x26
+   Enter = 0x1C
+   Z=0x2C
+   X=0x2D
+   C=0x2E
+   V=0x2F
+   B=0x30
+   N=0x31
+   M=0x32
+   Period = 0x34
+   Lctrl= 0x1D
+   Space = 0x39
+   Escape = 0x01
 
 PUL = ctypes.POINTER(ctypes.c_ulong)
 
@@ -95,42 +96,44 @@ class Input(ctypes.Structure):
    _fields_ = [("type", ctypes.c_ulong),
 ("ii", Input_I)]
 
-def set_pos(x, y):
-    x = 1 + int(x * 65536./computerWidth)
-    y = 1 + int(y * 65536./computerHeight)
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(x, y, 0, (0x0001 | 0x8000), 0, ctypes.pointer(extra))
-    command = Input(ctypes.c_ulong(0), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
+def setPos(location):
+   x=location[0]
+   y=location[1]
+   x = 1 + int(x * 65536./computerWidth)
+   y = 1 + int(y * 65536./computerHeight)
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+   ii_.mi = MouseInput(x, y, 0, (0x0001 | 0x8000), 0, ctypes.pointer(extra))
+   command = Input(ctypes.c_ulong(0), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(command), ctypes.sizeof(command))
  
 def left_click():
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(0, 0, 0, MouseButton.down.left, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(0), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+   ii_.mi = MouseInput(0, 0, 0, MouseButton.down.left, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(0), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
  
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(0, 0, 0, MouseButton.up.left, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(0), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+   ii_.mi = MouseInput(0, 0, 0, MouseButton.up.left, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(0), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 def right_click():
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(0, 0, 0, MouseButton.down.right, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(0), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+   ii_.mi = MouseInput(0, 0, 0, MouseButton.down.right, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(0), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
  
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.mi = MouseInput(0, 0, 0, MouseButton.up.right, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(0), ii_)
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))    
+   extra = ctypes.c_ulong(0)
+   ii_ = Input_I()
+   ii_.mi = MouseInput(0, 0, 0, MouseButton.up.right, 0, ctypes.pointer(extra))
+   x = Input(ctypes.c_ulong(0), ii_)
+   ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))    
 
-def press_key(key):
+def holdKey(key):
    extra = ctypes.c_ulong(0)
    ii_ = Input_I()
 
@@ -140,7 +143,7 @@ def press_key(key):
    x = Input(ctypes.c_ulong(1), ii_)
    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-def release_key(key):
+def releaseKey(key):
    extra = ctypes.c_ulong(0)
    ii_ = Input_I()
 
@@ -149,3 +152,17 @@ def release_key(key):
    ii_.ki = KeyBdInput(0, key, flags, 0, ctypes.pointer(extra))
    x = Input(ctypes.c_ulong(1), ii_)
    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def getPixel(location):
+	return PIL.ImageGrab.grab().load()[location[0], location[1]]
+def click(location):
+   setPos(location)
+   left_click()
+def rightClick(location):
+   setPos(location)
+   right_click()
+def pressKey(key):
+   holdKey(key)
+   time.sleep(0.05)
+   releaseKey(key)
+
